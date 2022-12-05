4658ecca0b7cddce2018333af6613a854845af0d
DucHighKing <119490992+DucHighKing@users.noreply.github.com>
2022-12-05 10:54:56 +0700

    ok

---
+++ defintion.js
@@ -0,0 +1,15 @@
+{
+  "type": "donhietdo",
+  "message0": "đo nhiệt độ nước",
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 160,
+  "tooltip": "",
+  "helpUrl": ""
+}
+
+Blockly.Python['donhietdo'] = function(block) {
+  // TODO: Assemble Python into code variable.
+  var code = '...\n';
+  return code;
+};
